83b3778e24ccb151e7a3984b88082b4237466474
RAVALI28 <ravalikavajinapally.rv@gmail.com>
2025-04-29 12:28:05 -0500

    Intial Commit

---
+++ MeatPriceTracker/src/main/java/com/talentflow/MeatPriceTracker/MeatPriceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.talentflow.MeatPriceTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatPriceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeatPriceTrackerApplication.class, args);
+	}
+
+}
---
+++ MeatPriceTracker/src/test/java/com/talentflow/MeatPriceTracker/MeatPriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentflow.MeatPriceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeatPriceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
