3fa8fb154b545db01758add9e039e0c91274b471
RAVALI28 <ravalikavajinapally.rv@gmail.com>
2025-04-30 13:00:31 -0500

    Added JWT Implementation to the products module and set  the security config respectively.

---
+++ MeatPriceTracker/src/main/java/com/talentflow/MeatPriceTracker/Utils/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.talentflow.MeatPriceTracker.Utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import static io.jsonwebtoken.Jwts.*;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            String token = authHeader.substring(7);
+
+
+            try {
+                Claims claims = Jwts.parser()
+                        .setSigningKey(jwtSecret)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                String email = claims.getSubject();
+
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(
+                                email,
+                                null,
+                                Collections.emptyList());
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            } catch (Exception exception) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or Expired Token");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
