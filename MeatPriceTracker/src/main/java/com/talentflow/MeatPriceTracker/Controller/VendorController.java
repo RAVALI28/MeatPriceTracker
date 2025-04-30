705a061b3a0c908fcd16672b329151519a0b163c
RAVALI28 <ravalikavajinapally.rv@gmail.com>
2025-04-30 14:41:56 -0500

    Added Vendor Module implementation with all CRUD Operations

---
+++ MeatPriceTracker/src/main/java/com/talentflow/MeatPriceTracker/Controller/VendorController.java
@@ -0,0 +1,38 @@
+package com.talentflow.MeatPriceTracker.Controller;
+
+import com.talentflow.MeatPriceTracker.Entity.Vendor;
+import com.talentflow.MeatPriceTracker.Service.VendorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/vendors")
+public class VendorController {
+
+    @Autowired
+    private VendorService vendorService;
+
+    @PostMapping
+    public Vendor createVendor(@RequestBody Vendor vendor){
+        return vendorService.createVendor(vendor);
+    }
+
+    @GetMapping
+    public List<Vendor> getAllvendors(){
+        return vendorService.getAllVendors();
+
+    }
+
+    @PutMapping("/{id}")
+    public Vendor updateVendor(@PathVariable Long id, @RequestBody Vendor vendor){
+        return vendorService.updateVendor(id, vendor);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteVendor(@PathVariable Long id){
+        vendorService.deleteVendor(id);
+        return "Vendor Successfully deleted";
+    }
+}
